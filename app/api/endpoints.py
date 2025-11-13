826fa588b83a0a05ff6a30712f9670a462a8c86f
alicranck <alicranck@gmail.com>
2025-11-13 09:46:40 +0200

    fast plotting, tracking, configs

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,33 +1,68 @@
 import yaml
+import time
 from fastapi import APIRouter, Query, HTTPException
 from fastapi.responses import StreamingResponse
 import cv2
 from ..ml_core.detector import OpenVocabularyDetector
+from ..utils.plotting import fast_plot
 
 current_dir = __file__.rsplit("/", 1)[0]
-with open(f"{current_dir}/../config.yaml", "r") as f:
+with open(f"{current_dir}/../ml_core/configs/ov_detection.yaml", "r") as f:
     config = yaml.safe_load(f)
 
 
 router = APIRouter()
 
 
-def video_frame_generator(detector: OpenVocabularyDetector, video_url: str):
+def stream_video_detection(detector: OpenVocabularyDetector, video_url: str):
     """
     Generator function that reads video frames, detects objects, and yields annotated JPEGs.
     """
 
     for results in detector.detect_stream(video_url):
-        
+
+        t0 = time.time()        
         if results.boxes:
-            annotated_frame = results.plot() # Use YOLO's plot function for simplicity
+            annotated_frame = fast_plot(results.orig_img, results.boxes, 
+                                        results.names)
         else:
             annotated_frame = results.orig_img
-
+        t1 = time.time()
         _, buffer = cv2.imencode('.jpg', annotated_frame, [cv2.IMWRITE_JPEG_QUALITY, 70])
-        
+        t2 = time.time()
+        print(f"[INFO] Plotting time: {(t1 - t0)*1000:.2f} ms | Encoding time: {(t2 - t1)*1000:.2f} ms")
         yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+        
+
+def video_detection(detector: OpenVocabularyDetector, video_url: str, vid_stride: int = 1):
+    """
+    Generator function that reads video frames, detects objects, and yields annotated JPEGs.
+    """
+
+    cap = cv2.VideoCapture(video_url)
+    if not cap.isOpened():
+        raise RuntimeError(f"Error opening video stream or file: {video_url}")
+
+    frame_count = 0
+    while True:
+        
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        if frame_count % vid_stride == 0:
+            results = detector.detect(frame, confidence_threshold=config['confidence_threshold'])[0]
+            annotated_frame = fast_plot(results.orig_img, results.boxes, results.names)
+            frame_count += 1        
+        else:
+            frame_count += 1
+            continue
+
+        _, buffer = cv2.imencode('.jpg', annotated_frame, [cv2.IMWRITE_JPEG_QUALITY, 70])
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+        
 
 
 @router.get("/stream/start")
@@ -41,10 +76,10 @@ async def start_detection_stream(
     class_list = [c.strip() for c in classes.split(',') if c.strip()]
     
     try:
-        detector = OpenVocabularyDetector(config['ov_detection']['model'])
+        detector = OpenVocabularyDetector(config['model'])
         detector.set_vocabulary(class_list)
         return StreamingResponse(
-            video_frame_generator(detector, video_url),
+            video_detection(detector, video_url, config.get('vid_stride', 1)),
             media_type="multipart/x-mixed-replace; boundary=frame"
         )
     except RuntimeError as e:
--- app/ml_core/detector.py
+++ app/ml_core/detector.py
@@ -2,8 +2,8 @@ import os
 from ultralytics import YOLOE # type: ignore
 from PIL.Image import Image as PILImage
 
-import openvino.properties.hint as hints
-from openvino import Core
+import onnxruntime as ort 
+
 
 current_dir = __file__.rsplit("/", 1)[0]
 
@@ -12,27 +12,30 @@ class OpenVocabularyDetector:
     def __init__(self, model_id: str):
         self.model_id = model_id
         self.model = self._init_model(model_id)
-        self.openvino_model = None
+        self.onnx_model = None
         self.vocab = []
 
     def set_vocabulary(self, vocab_list: list[str]):
         self.vocab = vocab_list
         pos_embeddings = self.model.get_text_pe(vocab_list)
         self.model.set_classes(self.vocab, pos_embeddings)
-        self.openvino_model = self.compile_openvino_model()
+        self.onnx_model = self.compile_onnx_model()
 
     def detect(self, image: PILImage, confidence_threshold: float = 0.25):
         # Move to ONNX for faster inference
-        assert self.openvino_model is not None, \
+        assert self.onnx_model is not None, \
                 "ONNX model is not initialized. Call set_vocabulary() first."
-        results = self.openvino_model.predict(image)
+        results = self.onnx_model.track(image, conf=confidence_threshold,
+                                        batch=1, imgsz=512, tracker='bytetrack.yaml')
         return results
     
     def detect_stream(self, video_url: str, confidence_threshold: float = 0.25):
                 # Move to ONNX for faster inference
-        assert self.openvino_model is not None, \
+        assert self.onnx_model is not None, \
                 "ONNX model is not initialized. Call set_vocabulary() first."
-        results = self.openvino_model.predict(source=video_url, stream=True)
+        results = self.onnx_model.track(source=video_url, stream=True,
+                                        batch=1, conf=confidence_threshold, 
+                                        vid_stride=3, imgsz=512, tracker='/home/alicranck/almog/projects/Flash-Findr/app/ml_core/configs/tracker.yaml')
         return results
 
     def _init_model(self, model_id: str):
@@ -44,15 +47,21 @@ class OpenVocabularyDetector:
             model = YOLOE(model_id)
 
         return model
+    
+    def compile_onnx_model(self):
+        onnx_model = self.model.export(format="onnx", dynamic=True,
+                                       imgsz=512, batch=1)
+        ort_session = YOLOE(onnx_model)
+        return ort_session
 
-    def compile_openvino_model(self):
-        core = Core()
-        config = {"PERFORMANCE_HINT": "THROUGHPUT",}
+    # def compile_openvino_model(self):
+    #     core = Core()
+    #     config = {"PERFORMANCE_HINT": "THROUGHPUT",}
 
-        self.model.export(format="openvino", half=True, dynamic=True, batch=16)
-        export_xml_path = os.path.join(current_dir, "../../models/",
-                                        self.model_id.split(".")[0] + "_openvino_model",
-                                          self.model_id.split(".")[0] + ".xml")
-        openvino_model = core.compile_model(export_xml_path, "CPU", config)
+    #     self.model.export(format="openvino", half=True, dynamic=True, batch=16)
+    #     export_xml_path = os.path.join(current_dir, "../../models/",
+    #                                     self.model_id.split(".")[0] + "_openvino_model",
+    #                                       self.model_id.split(".")[0] + ".xml")
+    #     openvino_model = core.compile_model(export_xml_path, "CPU", config)
         
-        return openvino_model
+    #     return openvino_model
